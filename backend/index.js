9a7a705a46d5f865d9e351554deabcc3f782d9be
AvineetKaur <avineetkaur15@gmail.com>
2024-07-22 16:17:15 +0530

    added zod validation

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,37 @@
 //with express.json() middleware
 
 const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express();
 
 app.use(express.json());
 
 app.get('/todos', function (req, res) {
 
+
+
 })
 app.post('/todo', function (req, res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
+    //put mongodb
 
 })
 app.put('/completed', function (req, res) {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
 
 })
 
