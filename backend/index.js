2171b85cd5fa0ce56e215e306588c6d1d0ca6ee4
AvineetKaur <avineetkaur15@gmail.com>
2024-07-22 22:45:59 +0530

    Backend urls added

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,25 @@
 //with express.json() middleware
 
 const express = require('express');
+const cors = require('cors');
 const { createTodo, updateTodo } = require('./types');
+const { todo } = require('./db');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/todos', function (req, res) {
+app.get('/todos', async function (req, res) {
+    const todos = await todo.find({});
+    console.log(todos);
+    res.json({
+        "todos": todos
+    })
 
 
 
 })
-app.post('/todo', function (req, res) {
+app.post('/todo', async function (req, res) {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if (!parsedPayload.success) {
@@ -21,10 +29,20 @@ app.post('/todo', function (req, res) {
         })
         return;
     }
-    //put mongodb
+    //put into mongodb
+
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+    res.json({
+        msg: "To Do created successfully"
+    })
+
 
 })
-app.put('/completed', function (req, res) {
+app.put('/completed', async function (req, res) {
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
     if (!parsedPayload.success) {
@@ -33,6 +51,16 @@ app.put('/completed', function (req, res) {
         })
         return;
     }
+
+    await todo.update({
+        _id: req.body.id
+    }, {
+        completed: true
+    })
+
+    res.json({
+        msg: "Todo marked is completed"
+    })
 
 })
 
